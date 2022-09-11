cdc57c0d4c37f25b23d6680fc32f008da67a2f8f
vinayak1809 <vinayakrshinde18@gmail.com>
2022-09-11 20:37:37 +0530

    relation between user and expense

--- app.js
+++ app.js
@@ -5,8 +5,6 @@ const dotenv = require("dotenv");
 const jwt = require("jsonwebtoken");
 const cors = require("cors");
 const store = require("store");
-// var LocalStorage = require("node-localstorage").LocalStorage,
-//   localStorage = new LocalStorage("./scratch");
 
 const app = express();
 
@@ -21,33 +19,19 @@ app.use(cors());
 
 dotenv.config();
 
-// const authenticateTheToken = (req, res, next) => {
-//   // const token =
-//   //   req.body.token || req.query.token || req.headers["x-access-token"];
-//   // console.log(store.get("token"), "ddd");
-//   // token = store.get("token");
-//   const authHeader = req.headers["authorization"];
-//   const token = authHeader && authHeader.split(" ")[1];
-
-//   console.log(authHeader);
-//   if (!token) {
-//     return res.status(403).send("A token is required for authentication");
-//   } else {
-//     const decoded = jwt.verify(token, process.env.TOKEN_SECRET);
-//     console.log(decoded, "decoded");
-//     req.user = decoded;
-//   }
-// };
-// // module.exports = verifyToken;
-// app.use("/add-expense", authenticateTheToken);
-//models
-const User = require("./api/server/src/models/user");
-const sequelize = require("./api/server/util/database");
-
 //routes
 const userRoutes = require("./api/server/routes/user");
 
 app.use(userRoutes);
+
+//models
+const sequelize = require("./api/server/util/database");
+
+const User = require("./api/server/src/models/user");
+const Expense = require("./api/server/src/models/expenses");
+
+User.hasMany(Expense);
+Expense.belongsTo(User);
 
 sequelize
   .sync()
