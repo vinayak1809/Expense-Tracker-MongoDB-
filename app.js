0f4863721e4ce8d8757717e1a1aaaa078726ebfa
vinayak1809 <vinayakrshinde18@gmail.com>
2022-09-15 00:47:34 +0530

    models and association

--- app.js
+++ app.js
@@ -2,16 +2,13 @@ const express = require("express");
 const path = require("path");
 const bodyParser = require("body-parser");
 const dotenv = require("dotenv");
-const jwt = require("jsonwebtoken");
 const cors = require("cors");
-const store = require("store");
-
 const app = express();
 
 app.use(express.static(path.join(__dirname, "/api/server/public")));
 
-app.set("view engine", "ejs");
-app.set("views", path.join(__dirname, "/api/server/views"));
+//app.set("view engine", "ejs");
+//app.set("views", path.join(__dirname, "/api/server/views"));
 
 // app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
@@ -19,19 +16,32 @@ app.use(cors());
 
 dotenv.config();
 
+////////////////////////////////////////////////////
 //routes
-const userRoutes = require("./api/server/routes/user");
+////////////////////////////////////////////////////
 
+const userRoutes = require("./api/server/routes/user");
 app.use(userRoutes);
 
+////////////////////////////////////////////////////
 //models
+////////////////////////////////////////////////////
+
 const sequelize = require("./api/server/util/database");
 
 const User = require("./api/server/src/models/user");
 const Expense = require("./api/server/src/models/expenses");
+const Order = require("./api/server/src/models/order");
+const ForgetPassword = require("./api/server/src/models/forgetPassword");
 
 User.hasMany(Expense);
 Expense.belongsTo(User);
+
+User.hasMany(Order);
+Order.belongsTo(User);
+
+User.hasMany(ForgetPassword);
+ForgetPassword.belongsTo(User);
 
 sequelize
   .sync()
