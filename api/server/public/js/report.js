551013d894bf1c23791eeef4725375e8963e9e3c
vinayak1809 <vinayakrshinde18@gmail.com>
2022-09-16 14:02:26 +0530

    upload file to AWS S3

--- api/server/public/js/report.js
+++ api/server/public/js/report.js
@@ -1,6 +1,36 @@
-const { default: axios } = require("axios");
+const token = localStorage.getItem("token");
+
 const url = "http://localhost:4000";
 
-window.addEventListener("DOMContentLoaded", () => {
-  axios.get(`${url}/report`);
-});
+// window.addEventListener("DOMContentLoaded", async () => {
+//   await axios
+//     .get(`${url}/report`, {
+//       headers: { authorization: token },
+//     })
+//     .then((result) => {
+//       console.log(result.data);
+//     });
+// });
+
+async function downloadbtn() {
+  const done = await axios
+    .get(`${url}/download`, {
+      headers: { authorization: token },
+    })
+    .then((response) => {
+      console.log(response, "response");
+      if (response.status === 200) {
+        const link = document.getElementById("link");
+
+        var anchorTag = document.createElement("a");
+        anchorTag.href = response.data.fileUrl;
+        anchorTag.innerHTML = "Click on link to download the file";
+        link.appendChild(anchorTag);
+      } else {
+        console.log("response.data.message");
+      }
+    })
+    .catch((err) => {
+      console.log("errrr:", err.status);
+    });
+}
